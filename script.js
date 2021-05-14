cd02d6657cb0dcc9324e858d96142d53819fd421
yumyumgood-127 <79446298+yumyumgood-127@users.noreply.github.com>
2021-05-14 10:59:19 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var slideWrapper = document.getElementById('slider-wrap');
+//current slideIndexition
+var slideIndex = 0;
+//items
+var slides = document.querySelectorAll('#slider-wrap ul li');
+//number of slides
+var totalSlides = slides.length;
+//get the slide width
+var sliderWidth = slideWrapper.clientWidth;
+//set width of items
+slides.forEach(function (element) {
+    element.style.width = sliderWidth + 'px';
+})
+//set width to be 'x' times the number of slides
+var slider = document.querySelector('#slider-wrap ul#slider');
+slider.style.width = sliderWidth * totalSlides + 'px';
+
+// next, prev
+var nextBtn = document.getElementById('next');
+var prevBtn = document.getElementById('previous');
+nextBtn.addEventListener('click', function () {
+    plusSlides(1);
+});
+prevBtn.addEventListener('click', function () {
+    plusSlides(-1);
+});
+
+function showSlides(n) {
+    slideIndex = n;
+    if (slideIndex == -1) {
+        slideIndex = totalSlides - 1;
+    } else if (slideIndex === totalSlides) {
+        slideIndex = 0;
+    }
+
+    slider.style.left = -(sliderWidth * slideIndex) + 'px';
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex + n);
+}
